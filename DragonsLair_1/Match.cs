b185eb5e0919d827be7f9e076d7dda54b4e3bca8
Nicolai Overby Andersen <nico706a@edu.eal.dk>
2017-10-26 08:57:38 +0200

    Add project files.

---
+++ DragonsLair_1/Match.cs
@@ -0,0 +1,10 @@
+﻿namespace DragonsLair_1
+{
+    public class Match
+    {
+        public Team FirstOpponent { get; set; }
+        public Team SecondOpponent { get; set; }
+        public Team Winner { get; set; } = null;
+       
+    }
+}
---
+++ DragonsLair_1/Round.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+
+namespace DragonsLair_1
+{
+    public class Round
+    {
+        private List<Match> matches = new List<Match>();
+        
+        public void AddMatch(Match m)
+        {
+            matches.Add(m);
+        }
+
+        public Match GetMatch(string teamName1, string teamName2)
+        {
+            //har indsat en ny instans
+            Match match = new Match();
+
+            
+            // TODO: Implement this method
+            return match;
+        }
+
+        public bool IsMatchesFinished()
+        {
+            bool isFinished = false;
+
+            foreach (Match match in matches)
+            {
+                if (matches != null)
+                {
+                    isFinished = true;
+                }
+            }
+            return isFinished;
+        }
+            
+        public List<Team> GetWinningTeams()
+        {
+            List<Team> winner = new List<Team>();
+
+            foreach (Match match in matches)
+            {
+                    winner.Add(match.Winner);
+            }
+
+            
+            return winner;
+        }
+
+        public List<Team> GetLosingTeams()
+        {
+            //Compare if one team did not win
+            List<Team> loser = new List<Team>();
+
+            foreach(Match match in matches)
+            {
+                //Instansering.
+                if(match.FirstOpponent.Name != match.Winner.Name)
+                {
+                    loser.Add(match.FirstOpponent);
+                }
+                else
+                {
+                    loser.Add(match.SecondOpponent);
+                }
+                
+            }
+
+            return loser;
+        }
+    }
+}
